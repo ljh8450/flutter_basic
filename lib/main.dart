f8d3e0d3be058e743562dd8e779610b00dd1ef62
BOOK-R4R6F38DIQ <ljh8450@ajou.ac.kr>
2025-05-22 07:32:39 +0900

    make memo baseline

--- lib/main.dart
+++ lib/main.dart
@@ -12,101 +12,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Instagram', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.black,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(15),
-                color: Colors.grey[300],
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        'https://placecats.com/neo/100/100',
-                      ),
-                    ),
-                    SizedBox(width: 15),
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        'https://placecats.com/millie/100/100',
-                      ),
-                    ),
-                    SizedBox(width: 15),
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        'https://placecats.com/bella/100/100',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 8,
-              child: Container(
-                color: Colors.grey[100],
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(15),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 30,
-                            child: FlutterLogo(size: 30),
-                          ),
-                          SizedBox(width: 15),
-                          Text(
-                            '반가반가 코딩',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Spacer(),
-                          Icon(Icons.more_horiz),
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        padding: EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(
-                              'https://placecats.com/neo_banana/300/300',
-                            ),
-                            SizedBox(height: 15),
-                            Row(
-                              children: [
-                                Icon(Icons.favorite_border),
-                                SizedBox(width: 8.0),
-                                Icon(Icons.comment),
-                                SizedBox(width: 8.0),
-                                Icon(Icons.send),
-                                Spacer(),
-                                Icon(Icons.bookmark_border),
-                              ],
-                            ),
-                            Text(
-                              'Linked by username1, username2, and 100 others',
-                            ),
-                            Text('View all 20 Comments'),
-                            Text('2 hours ago'),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+          title: Text('Memo'),
+          backgroundColor: Colors.amber,
+          actions: [
+            TextButton(
+              onPressed: () {
+                print('클릭');
+              },
+              child: Text('버튼'),
             ),
           ],
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('아이템 $index'),
+              tileColor: Colors.amber[100],
+              trailing: IconButton(
+                onPressed: () {
+                  print('삭제: 아이템 $index');
+                },
+                icon: Icon(Icons.delete),
+              ),
+            );
+          },
         ),
       ),
     );
